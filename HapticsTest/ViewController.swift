62e5cd5e4d21b926db6536accc2f580e6f1307b0
Juan Patten <jr@juanpatten.com>
2017-07-08 15:47:30 -0600

    Add UIImpactFeedbackStyle variations

--- HapticsTest/ViewController.swift
+++ HapticsTest/ViewController.swift
@@ -21,23 +21,25 @@ class ViewController: UITableViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
     //MARK: - table view
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 4
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
+        case 0:
+            return 3
         case 2:
             return 3
         default:
             return 1
         }
     }
-    
+
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0:
@@ -52,12 +54,21 @@ class ViewController: UITableViewController
             return nil
         }
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         switch indexPath.section {
         case 0:
-            cell.textLabel?.text = "Trigger impact feedback"
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "Trigger impact feedback (light)"
+            case 1:
+                cell.textLabel?.text = "Trigger impact feedback (medium)"
+            case 2:
+                cell.textLabel?.text = "Trigger impact feedback (heavy)"
+            default:
+                break
+            }
         case 1:
             cell.textLabel?.text = "Trigger selection feedback"
         case 2:
@@ -78,11 +89,22 @@ class ViewController: UITableViewController
         }
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.section {
         case 0:
-            triggerImpactFeedback()
+            let weight: UIImpactFeedbackStyle
+            switch indexPath.row {
+            case 0:
+                weight = .light
+            case 1:
+                weight = .medium
+            case 2:
+                weight = .heavy
+            default:
+                weight = .medium
+            }
+            triggerImpactFeedback(weight: weight)
         case 1:
             triggerSelectionFeedback()
         case 2:
@@ -106,40 +128,40 @@ class ViewController: UITableViewController
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    
-    
+
+
     //MARK: - haptic methods
-    func triggerImpactFeedback(){
-        let feedback = UIImpactFeedbackGenerator()
-        
+    func triggerImpactFeedback(weight: UIImpactFeedbackStyle){
+        let feedback = UIImpactFeedbackGenerator(style: weight)
+
         feedback.prepare()
-        
-        /*NOTE: 
+
+        /*NOTE:
          This is not the best example for the usage of prepar.
-        Apple says in it's doc that you should use prepare at short time before you expect the impact to fire to reduce latency
-        In this case for simplicity we just fire when it's done
-        For more info see the doc: https://developer.apple.com/reference/uikit/uifeedbackgenerator */
-        
+         Apple says in it's doc that you should use prepare at short time before you expect the impact to fire to reduce latency
+         In this case for simplicity we just fire when it's done
+         For more info see the doc: https://developer.apple.com/reference/uikit/uifeedbackgenerator */
+
         feedback.impactOccurred()
     }
-    
+
     func triggerSelectionFeedback(){
         let feedback = UISelectionFeedbackGenerator()
         feedback.prepare()
         feedback.selectionChanged()
     }
-    
+
     func triggerNotifactionHapticForType(type:UINotificationFeedbackType){
         let feedback = UINotificationFeedbackGenerator()
         feedback.prepare()
         feedback.notificationOccurred(type)
     }
-    
-    func triggerGoodOldVibration(){
-         AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-    }
-    
-    
 
+    func triggerGoodOldVibration(){
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+
+    
+    
 }
 
